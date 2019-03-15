5b07a19891f5b7a8e7dae82c023a5372191edccf
Sergey <sen.ser@mail.ru>
2019-03-15 15:18:10 +0300

    Make validate.

--- src/code.js
+++ src/code.js
@@ -3,8 +3,9 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import '@fancyapps/fancybox';
 import WOW from'WOWjs';
+import Inputmask from "inputmask";
+import valid from "jquery-validation";
 //import 'lazyYT/lazyYT.js';
-import './fonts/fonts.css';
 
 ymaps.ready(function () {
     var myMap = new ymaps.Map('map', {
@@ -42,21 +43,23 @@ $('.zayv').click(()=> {
 $('.menu-gamb').click(function() {
 	$(this).next().toggleClass('menu_height');
 })
+
+
 var slider = false;
+var image = $('.main__pic_img');
+var slick = $(".main__pic");
 if (window.matchMedia("(max-width: 976px)").matches) {
 		slider = true;
-		var image = $('.main__pic_img');
 		image.remove();
-		var slick = $(".main__pic").slick({
+		slick.slick({
 			dots: true
 		});
 }
 
 $(window).resize(function() {
 	if (window.matchMedia("(max-width: 976px)").matches && !slider) {
-		var image = $('.main__pic_img');
 		image.remove();
-		var slick = $(".main__pic").slick({
+		slick.slick({
 			dots: true
 		});
 		slider = true;
@@ -68,3 +71,77 @@ if (window.matchMedia("(min-width: 1243px)").matches && !slider) {
 		 mobile: true
 	}).init();
 }
+
+
+$('.menu__item').click(function () {
+    $('html, body').animate({
+        scrollTop: $('.' + $(this).attr('href').substr(1)).offset().top
+    }, 500);
+
+    return false;
+});
+
+Inputmask("+7 (999) 999-99-99").mask("input[type='tel']");
+
+$('form').submit(function() {
+	var form = $(this);
+	var error = false;
+	
+	var nameEl = form.find("input[name=name]");
+	var name = nameEl.val().trim();
+	if (name === '') {
+		nameEl.css('border', '1px solid red');
+		error = true;
+	} else nameEl.removeAttr('style');
+	var tel = form.find("input[type='tel']");
+	var number = tel.val().trim();
+	var reg = /^([\+]+)*[0-9\x20\x28\x29\-]{5,20}$/;
+	if(reg.test(number) == false) {
+		tel.css('border', '1px solid red');
+		error = true;
+	} else tel.removeAttr('style');
+
+	var email = form.find("input[type='email']");
+	var adress = email.val().trim();
+	var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+	if(reg.test(adress) == false) {
+		email.css('border', '1px solid red');
+		error = true;
+	} else email.removeAttr('style');
+
+
+	var checkboxEl = form.find("input[name='query']");
+	var labelEl = form.find('label');
+	if (!$(checkboxEl).prop( "checked")) {
+		labelEl.css('color', 'red');
+		error = true;
+	}else labelEl.removeAttr('style');
+
+	var adress = email.val().trim();
+	var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+	if(reg.test(adress) == false) {
+		email.css('border', '1px solid red');
+		error = true;
+	} else email.removeAttr('style');
+
+
+	if (error) return false;
+
+	// $.ajax({
+	//     url: form.attr('action'),
+	//     method: 'post',
+	//     dataType: 'html',
+	//     data: {
+	//     		name: name,
+	//     		tel: number,
+	//     		email: adress
+	//     	},
+	//     success: function(data){
+	//         alert(data);
+ //    	}
+	// });
+	form.trigger('reset');
+	$.fancybox.close($('.modal-z'));
+	$.fancybox.open($('.modal-thanks'));
+	return false;
+})
